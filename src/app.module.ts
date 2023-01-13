fbe192efef50341fa95101f436f17a50ea1944d8
Vregi Dev <fb_dev@ml.wfc.vn>
2023-01-13 16:31:15 +0700

    doing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,25 @@
-import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ArticleModule } from './article/article.module';
 import { UserModule } from './user/user.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
 import { ProfileModule } from './profile/profile.module';
 import { TagModule } from './tag/tag.module';
+import { TasksModule } from './tasks/tasks.module';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot(typeOrmConfig),
     // ArticleModule,
     // UserModule,
     // ProfileModule,
-    // TagModule,
+    // TagModule
     TasksModule
-
   ],
   controllers: [
     // AppController
   ],
   providers: []
 })
-export class ApplicationModule {
-  // constructor(private readonly connection: Connection) {}
-}
+export class ApplicationModule { }
