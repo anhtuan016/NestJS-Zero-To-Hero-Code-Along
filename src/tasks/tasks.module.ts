0df68cec41f6b85fded4d0e8515daa03843b43d5
Vregi Dev <fb_dev@ml.wfc.vn>
2023-02-06 09:18:14 +0700

    doing

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from './../auth/auth.module';
 import { TaskRepository } from './task.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
@@ -6,7 +7,10 @@ import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [
+    TypeOrmModule.forFeature([Task]),
+    AuthModule
+  ],
   controllers: [TasksController],
   providers: [TasksService, TaskRepository],
 })
